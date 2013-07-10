6e16597a26e524e503448f5d7765efc84bd4ada6
araujodavid <david.brasillo@gmail.com>
2013-07-10 10:03:22 -0300

    Adicionar o collector NetworkTrafficCollector

---
+++ mydbaas-agent/src/main/java/br/com/arida/ufc/mydbaas/agent/collector/database/NetworkTrafficCollector.java
@@ -0,0 +1,28 @@
+package main.java.br.com.arida.ufc.mydbaas.agent.collector.database;
+
+import main.java.br.com.arida.ufc.mydbaas.agent.collector.common.AbstractCollector;
+import main.java.br.com.arida.ufc.mydbaas.agent.entity.NetworkTrafficMetric;
+
+/**
+ * @author David Araújo - @araujodavid
+ * @version 1.0
+ * @since July 10, 2013
+ */
+public class NetworkTrafficCollector extends AbstractCollector<NetworkTrafficMetric> {
+
+	public NetworkTrafficCollector(int identifier, String type) {
+		super(identifier, type);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void loadMetric(Object[] args) throws Exception {
+		// TODO Auto-generated method stub		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub		
+	}
+
+}

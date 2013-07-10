264428612cde96ff257a8237f10da83989c9c814
araujodavid <david.brasillo@gmail.com>
2013-07-10 10:04:05 -0300

    Adicionar o collector StatementCollector

---
+++ mydbaas-agent/src/main/java/br/com/arida/ufc/mydbaas/agent/collector/database/StatementCollector.java
@@ -0,0 +1,28 @@
+package main.java.br.com.arida.ufc.mydbaas.agent.collector.database;
+
+import main.java.br.com.arida.ufc.mydbaas.agent.collector.common.AbstractCollector;
+import main.java.br.com.arida.ufc.mydbaas.agent.entity.StatementMetric;
+
+/**
+ * @author David Araújo - @araujodavid
+ * @version 1.0
+ * @since July 10, 2013
+ */
+public class StatementCollector extends AbstractCollector<StatementMetric> {
+
+	public StatementCollector(int identifier, String type) {
+		super(identifier, type);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void loadMetric(Object[] args) throws Exception {
+		// TODO Auto-generated method stub		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub		
+	}
+
+}
